2cf35d7da6884f448ccaa382029ff620feed62ae
Ava Edwards <ava.edwards09@proton.me>
2024-09-07 11:30:26 +0800

    working

--- update_main_config.zsh
+++ update_main_config.zsh
@@ -8,12 +8,16 @@ C_YELLOW="\033[38;5;11m"
 source=$1
 destination=$2
 
-#check if source and destination paths are provided
-if [[ $# < 2 ]]; then
-  echo -e "${C_RED}Error: source and destination paths are required${NO_FORMAT}"
-  exit 1
-elif [[ $# > 2 ]]; then
+
+#check too many arguments
+if [[ $# -gt 1 ]]; then
   echo -e "${C_RED}Error: too many arguments${NO_FORMAT}"
+  exit 1
+fi
+
+#check if source path is provided
+if [[ $# -ne ]] 1; then
+  echo -e "${C_RED}Error: source path is required${NO_FORMAT}"
   exit 1
 fi
 
@@ -32,19 +36,15 @@ check_dir_path() {
 }
 
 check_dir_path "$1" "source"
-check_dir_path "$2" "destination"
+
 
 if [[ ! -e "$1/.obsidian" ]]; then
   echo -e "${C_RED}Error: source directory does not contain .obsidian folder${NO_FORMAT}"
   exit 1
 fi
 
-if [[ ! -e "$2/.obsidian" ]]; then
-  echo -e "${C_YELLOW}Creating .obsidian folder in destination directory${NO_FORMAT}"
-  mkdir "$2/.obsidian"
-fi
 
-rm -rf "$2/.obsidian"
-cp -r "$1/.obsidian" "$2"
-rm "$2/.obsidian/workspace.json"
-touch "$2/.obsidian/workspace.json"
+rm -rf "/Users/saxophonin/Personal/programming/obsidian_config_manager/main_config/.obsidian"
+cp -r "$1/.obsidian" "/Users/saxophonin/Personal/programming/obsidian_config_manager/main_config"
+rm "/Users/saxophonin/Personal/programming/obsidian_config_manager/main_config/.obsidian/workspace.json"
+touch "/Users/saxophonin/Personal/programming/obsidian_config_manager/main_config/.obsidian/workspace.json"
