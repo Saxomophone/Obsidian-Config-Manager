4b036a441f7c2f1fe178d5d1b45c31369c5fe7e9
Ava Edwards <ava.edwards09@proton.me>
2024-09-10 15:15:21 +0800

    now handles keyboard interrupt

--- init.sh
+++ init.sh
@@ -6,8 +6,12 @@ C_YELLOW="\033[38;5;11m"
 
 function clean() {
   rm -rf ~/.config/obsidian_config_manager/
+  trap SIGINT
+  exit $1
 }
 
+
+trap "clean 1" INT # clean up if user exits with ctrl-c
 
 # if ~/.config/obsidian_config_manager does not exist, create it
 if [[ ! -e ~/.config/obsidian_config_manager ]]; then
@@ -24,14 +28,16 @@ read source
 if [[ $source == "g" ]]; then
   echo -e "Enter the github url of the main_config"
   read github_url
-  git clone $github_url ~/.config/obsidian_config_manager/main_config || (echo -e "${C_RED}Error: invalid github url${NO_FORMAT}" && clean && exit 1)
+  git clone $github_url ~/.config/obsidian_config_manager/main_config || (echo -e "${C_RED}Error: invalid github url${NO_FORMAT}" && clean 1)
 elif [[ $source == "l" ]]; then
   echo -e "Enter the path to the main_config"
-  read path
+  read -e path
   mkdir -p ~/.config/obsidian_config_manager/main_config/.obsidian
-  cp -r "$path" ~/.config/obsidian_config_manager/main_config/.obsidian/ || (echo -e "${C_RED}Error: invalid path${NO_FORMAT}" && clean && exit 1)
+  cp -r "$path" ~/.config/obsidian_config_manager/main_config/.obsidian/ || (echo -e "${C_RED}Error: invalid path${NO_FORMAT}" && clean 1)
 else
   echo -e "${C_RED}Error: invalid source${NO_FORMAT}"
-  clean
-  exit 1
+  clean 1
 fi
+
+
+trap SIGINT # remove trap and give user control back
